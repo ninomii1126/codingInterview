ba2e329a6ad8995e961e4ffe4c39219a43598275
Joyce Tsai <ninomii1126@gmail.com>
2023-08-09 15:13:47 -0700

    swap nodes in linked list

---
+++ Swap Nodes/SwapNodes.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class SwapNodes{
+    public static LinkedListNode swapNodes(LinkedListNode head, int k) {
+
+    // Replace this placeholder return statement with your code
+
+    LinkedListNode curr = head;
+    int count = 0;
+
+    LinkedListNode front = null;
+    LinkedListNode end = null;
+
+    while(curr != null){
+      count++;
+
+      if(count == k){
+        front = curr;
+        end = head;
+      }
+
+      if(count>k){
+        end = end.next;
+      }
+
+      curr = curr.next;
+    }
+
+  
+    // swap values
+    int endValue = end.data;
+    int frontValue = front.data;
+
+    front.data = endValue;
+    end.data = frontValue;
+      
+    return head;
+  }
+}
